6a3437f1edf2998f5dec476d16443eef985286a7
StreetCodeNinja <StreetCodeNinja@gmail.com>
2023-09-29 17:48:13 +0200

    Server Push

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo mv easypanel.sh /opt/lampp/easypanel.sh
+sudo mv xampp_icon.png /opt/lampp/xampp_icon.png
+sudo cp xampp.desktop /usr/share/applications
+mv xampp.desktop ~/Escritorio/xampp.desktop
+echo done!
